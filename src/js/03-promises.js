029c0a6744f2aa9172df13cba1554c229433eeb7
PPacuk <piotrek.pacuk@gmail.com>
2022-12-18 16:28:01 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,10 @@
+const firstDelay = document.querySelector("input[name='delay']");
+const delayStep = document.querySelector("input[name='step']");
+const amount = document.querySelector("input[name='amount']");
+const btn = document.querySelector("button");
+
+console.log(amount);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
