966cb2b83b995a28a71def13f6cc86b79c28f27c
PPacuk <piotrek.pacuk@gmail.com>
2022-12-18 10:32:22 +0100

    01-09 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let i = 0;
+let intervalId = null;
+
+const startInterval = () => {
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor(i++);
+  }, 1000);
+
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+};
+
+const stopInterval = () => {
+  clearInterval(intervalId);
+
+  stopBtn.setAttribute('disabled', true);
+  startBtn.removeAttribute('disabled');
+};
+
+startBtn.addEventListener('click', startInterval);
+stopBtn.addEventListener('click', stopInterval);
