0022e0e983279cdff0768eacfe25fb9915b56ae6
PPacuk <105542652+PPacuk@users.noreply.github.com>
2022-12-19 20:25:40 +0000

    Deploying to gh-pages from @ PPacuk/goit-js-hw-09@207e5f37bf801fb95287b9da7ebd6cd8b58c2aea 🚀

--- 03-promises.c629eea1.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("input[name='delay']"),document.querySelector("input[name='step']");var e=document.querySelector("input[name='amount']");document.querySelector("button");console.log(e)}();
-//# sourceMappingURL=03-promises.c629eea1.js.map
